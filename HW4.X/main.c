94e756f2ff961f225d50d334e7efec8c26243515
maxthecreator <49130042+maxthecreator@users.noreply.github.com>
2019-04-15 11:15:07 -0500

    delete ram stuff.

--- HW4.X/main.c
+++ HW4.X/main.c
@@ -117,12 +117,215 @@ int main() {
     TRISBbits.TRISB4 = 0;
     LATAbits.LATA4 = 1;
     
+    // Demonstrates spi by accessing external ram
+
+// PIC is the master, ram is the slave
+
+// Uses microchip 23K256 ram chip (see the data sheet for protocol details)
+
+// SDO4 -> SI (pin F5 -> pin 5)
+    RPB2Rbits.RPB2R = 0b0100  //SETS RPB2 (PIN 6) TO SDO2
+// SDI4 -> SO (pin F4 -> pin 2)
+    SDI2Rbits.SDI2R = 0b0011 //SETS SDI2 TO RPB13 (PIN 24)
+    
+// SCK4 -> SCK (pin B14 -> pin 6)   DONE
+
+// SS4 -> CS (pin B8 -> pin 1)      DONE
+            TRISBbits.TRISB8 = 0
+            CS = 1
+            
+// Additional SRAM connections
+
+// Vss (Pin 4) -> ground            DONE
+
+// Vcc (Pin 8) -> 3.3 V             DONE   
+
+// Hold (pin 7) -> 3.3 V (we don't use the hold function)       DONE
+
+// 
+
+// Only uses the SRAM's sequential mode
+
+//
+
+#define CS LATBbits.LATB8       // chip select pin
+
+
+
+// send a byte via spi and return the response
+
+unsigned char spi_io(unsigned char o) {
+
+  SPI1BUF = o;
+
+  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+
+    ;
+
+  }
+
+  return SPI4BUF;
+
+}
+
+
+
+
+  TRISBbits.TRISB8 = 0;
+
+  CS = 1;
+
+
+  SPI1CON = 0;              // turn off the spi module and reset it
+
+  SPI1BUF;                  // clear the rx buffer by reading from it
+
+  SPI1BRG = 0x3;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
+
+  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+
+  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+
+  SPI1CONbits.MSTEN = 1;    // master operation
+
+  SPI1CONbits.ON = 1;       // turn on spi 4
+
+
+
+                            // send a ram set status command.
+
+  CS = 0;                   // enable the ram
+
+  spi_io(0x01);             // ram write status
+
+  spi_io(0x41);             // sequential mode (mode = 0b01), hold disabled (hold = 0)
+
+  CS = 1;                   // finish the command
+
+}
+
+
+
+// write len bytes to the ram, starting at the address addr
+
+void ram_write(unsigned short addr, const char data[], int len) {
+
+  int i = 0;
+
+  CS = 0;                        // enable the ram by lowering the chip select line
+
+  spi_io(0x2);                   // sequential write operation
+
+  spi_io((addr & 0xFF00) >> 8 ); // most significant byte of address
+
+  spi_io(addr & 0x00FF);         // the least significant address byte
+
+  for(i = 0; i < len; ++i) {
+
+    spi_io(data[i]);
+
+  }
+
+  CS = 1;                        // raise the chip select line, ending communication
+
+}
+
+
+
+// read len bytes from ram, starting at the address addr
+
+void ram_read(unsigned short addr, char data[], int len) {
+
+  int i = 0;
+
+  CS = 0;
+
+  spi_io(0x3);                   // ram read operation
+
+  spi_io((addr & 0xFF00) >> 8);  // most significant address byte
+
+  spi_io(addr & 0x00FF);         // least significant address byte
+
+  for(i = 0; i < len; ++i) {
+
+    data[i] = spi_io(0);         // read in the data
+
+  }
+
+  CS = 1;
+
+}
+	init_spi();
+
+
+
+  while(1) {
+
+	_CPO_SET_COUNT(0);
+
+	float f = 512 +512*sin(i*2*3.1415/1000*10);  //should make a 10Hz sin wave)
+
+	i++;
+
+
+
+
+
+
+
+	setVoltage(0,512);		//test
+
+	setVoltage(1,256);		//test
+
+
+
+	while(_CPO_GET_COUNT() < 2400000000/1000) {}  //check this is 24Million
+
+    ;
+
+  }
+
+  return 0;
+
+}
+
+
+
+
+
+/*
+
+void setVoltage(char a, int v) {
+
+
+
+	unsigned short t = 0;
+
+	t= a << 15; //a is at the very end of the data transfer
+
+	t = t | 0b01110000000000000;
+
+	t = t | ((v&0b1111111111) <<2); //rejecting excessive bits (above 10)
+
+	
+
+	CS = 0;
+
+	spi_io(t>>8);
+
+	spi_
+
+	
+
+}
+    
+    */
     
     __builtin_enable_interrupts();
 
    // a4 led 
    // b4 pushbutton
-
+/*
     while(1) {
         
          
@@ -137,5 +340,5 @@ int main() {
 	// remember the core timer runs at half the sysclk
 
     }
-
+*/
 }
