751d20f0c4ab613c529c42ead04f4474e87d1d0e
maxthecreator <49130042+maxthecreator@users.noreply.github.com>
2019-04-18 16:51:44 -0500

    started hw5

---
+++ HW5.X/i2c.h
@@ -0,0 +1,24 @@
+
+#ifndef I2C_MASTER_NOINT_H__
+
+#define I2C_MASTER_NOINT_H__
+
+// Header file for i2c_master_noint.c
+
+// helps implement use I2C1 as a master without using interrupts
+
+void i2c_master_setup(void)
+
+void i2c_master_start(void);
+
+void i2c_master_restart(void);
+
+void i2c_master_send(unsigned char byte);
+
+unsigned char i2c_master_recv(void);
+
+void i2c_master_ack(int);
+
+void i2c_master_stop(void);
+
+#endif
