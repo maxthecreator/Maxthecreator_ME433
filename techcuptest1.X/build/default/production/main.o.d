404fb77a9f37f3a3d19108ba1f549691f0674b62
maxthecreator <49130042+maxthecreator@users.noreply.github.com>
2019-06-10 11:47:20 -0500

    techcup

---
+++ techcuptest1.X/build/default/production/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/main.o:  \
+main.c  \
+ili2.h  \
+imu.H 
---
+++ techcuptest1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=techcuptest1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/techcuptest1.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=techcuptest1.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/techcuptest1.x.tar
